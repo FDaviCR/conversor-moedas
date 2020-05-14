a46d10157aba90bcb3ca72ec5ea0b44c07cf2d38
Davi <davicamiloribeiro@gmail.com>
2020-05-14 20:48:42 -0300

    Estrutura HTML

--- src/App.js
+++ src/App.js
@@ -1,9 +1,56 @@
 import React from 'react';
 import './App.css';
+import {Jumbotron, Button, Form, Col, Spinner, Alert, Modal} from 'react-bootstrap';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faAngleDoubleRight} from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
-    <h1>Conversor de moedas!</h1>
+    <div style={{paddingTop:'10%'}}>
+      <h1 className="text-center">Conversor de moedas!</h1>
+      <Alert variant="danger" show={true}>
+        Erro, tente novamente!
+      </Alert>
+      <Jumbotron>
+        <Form>
+          <Form.Row>
+            <Col sm="3">
+              <Form.Control placeholder="0" value={1} required />
+            </Col>
+            <Col sm="3">
+              <Form.Control as="select">
+              </Form.Control>
+            </Col>
+            <Col sm="1" className="text-center" style={{paddingTop:'5px'}}>
+              <FontAwesomeIcon icon={faAngleDoubleRight}/>
+            </Col>
+            <Col sm="3">
+              <Form.Control as="select">
+              </Form.Control>
+            </Col>
+            <Col sm="2">
+              <Button variant="success" type="submit">
+                <Spinner animation="border" size="sm"/>
+                Converter
+              </Button>
+            </Col>
+          </Form.Row>
+        </Form>
+        <Modal show={true}>
+          <Modal.Header closeButton>
+            <Modal.Title>Conversão</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            Resultado...
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="success">
+              Nova Conversão
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </Jumbotron>
+    </div>
   );
 }
 
